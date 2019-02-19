bac4f66877acedad297ff50fae813d5ac7ed944f
lifang <z80645885@163.com>
2019-02-19 19:20:57 +0800

    add getSearchResult fn

--- NeteaseMusic/src/views/SearchPage.jsx
+++ NeteaseMusic/src/views/SearchPage.jsx
@@ -38,6 +38,14 @@ function SearchPage(props) {
     }
   }
 
+  function getSearchResult(e) {
+    console.log('getSearchResult...', e.target.dataset.name)
+    setShowSuggest(false);
+    setShowResult(true);
+    setShowHots(false);
+    props.searchResult(e.target.dataset.name);
+  }
+
   function goPlay(e) {
     if (e.target.tagName.toUpperCase() === 'P') {
       props.history.push({
@@ -56,10 +64,10 @@ function SearchPage(props) {
           </p>
           <span className={styles.search_back} onClick={() => window.history.back()}>取消</span>
         </header>
-        <div className={styles.search_suggest}>
+        <div className={styles.search_suggest} onClick={getSearchResult}>
           {showSuggest ? <div className={styles.suggest_cont}>{
             props.search.searchSuggest.map((item, index) => {
-              return <p className={styles.suggest_item} key={index}>{item.name}</p>
+              return <p className={styles.suggest_item} key={index} data-name={item.name}>{item.name}</p>
             })
           }</div> : null}
         </div>
@@ -67,11 +75,11 @@ function SearchPage(props) {
           <div className={styles.search_hots}>
             <div className={styles.hot_title}>
               热门搜索
-          </div>
-            <div className={styles.hot_cont}>
+            </div>
+            <div className={styles.hot_cont} onClick={getSearchResult}>
               {
                 props.search.searchHot.map((item, index) => {
-                  return <span className={styles.hot_tag} key={index}>{item.first}</span>
+                  return <span data-name={item.first} className={styles.hot_tag} key={index}>{item.first}</span>
                 })
               }
             </div>
